1c2d89f7ac08cb8f26b37f5b6df4e7a04132cdcf
sidheshsahu <sidhesh.sahu@somaiya.edu>
2026-08-15 07:39:58 +0530

    Added simple logic for answer relevancy

--- evaluation/simple.py
+++ evaluation/simple.py
@@ -1,22 +1,43 @@
 from deepeval.models import GeminiModel
 from deepeval.metrics import AnswerRelevancyMetric
 import os
+from deepeval import evaluate
 from deepeval.test_case import LLMTestCase
 from deepeval.metrics import AnswerRelevancyMetric
+from evaluation.q_and_a_list import questions,pred_ans
+
 
 model = GeminiModel(
     model="gemini-3.6-flash",
+    
     api_key=os.getenv('GOOGLE_API_KEY'),
     temperature=0.4,
 )
 
-answer_relevancy_metric = AnswerRelevancyMetric(model=model)
-test_case = LLMTestCase(
-  input="Who is the current president of the United States of America?",
-  actual_output="Joe Biden",
-  retrieval_context=["Joe Biden serves as the current president of America."]
+
+metric = AnswerRelevancyMetric(
+threshold=0.7,
+async_mode=False,
+model=model,
+include_reason=True
 )
-answer_relevancy_metric.measure(test_case)
-print(answer_relevancy_metric.score)
+
+print("ASYNC MODE:", metric.async_mode)
+
+test_cases = []
+
+for question, pred_ans in zip(questions, pred_ans):
+    test_case = LLMTestCase(
+        input=question,
+        actual_output=pred_ans,
+    )
+
+    test_cases.append(test_case)
+
+# Evaluate all test cases
+results = evaluate(
+    test_cases=test_cases,
+    metrics=[metric]
+)
 
 
