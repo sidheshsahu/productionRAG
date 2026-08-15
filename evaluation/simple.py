c1bfc372731d9ffa129fdee8e06bf895a7164093
sidheshsahu <sidhesh.sahu@somaiya.edu>
2026-08-15 10:57:35 +0530

    Set up confident ai

--- evaluation/simple.py
+++ evaluation/simple.py
@@ -9,7 +9,7 @@ from evaluation.q_and_a_list import questions,pred_ans
 import time
 
 model = GeminiModel(
-    model="gemini-3.6-flash",
+    model="gemini-3.7-flash",
     
     api_key=os.getenv('GOOGLE_API_KEY'),
     temperature=0.4,
@@ -42,24 +42,4 @@ for i, (question, pred_ans) in enumerate(zip(questions, pred_ans)):
     if i < len(questions) - 1: 
         print("Waiting 60 seconds before next question...") 
         time.sleep(60)
-
-# test_cases = []
-
-# for question, pred_ans in zip(questions, pred_ans):
-#     test_case = LLMTestCase(
-#         input=question,
-#         actual_output=pred_ans,
-#     )
-
-#     test_cases.append(test_case)
-
-# # Evaluate all test cases
-# results = evaluate(
-#     test_cases=test_cases,
-#     metrics=[metric],
-#     async_config=AsyncConfig(
-#         run_async=False
-#     )
-# )
-
 
